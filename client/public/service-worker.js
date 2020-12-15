e6cdabdfa19b86800e3b69eaf4388ce9e5bf5d95
Diego Hernandez <ishtaracademy@outlook.com>
2020-12-15 14:24:56 -0700

    Added .eslintcahe to git igonore; addes some clogs to service-worker to find an issue with api req not triggering the offline page

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -47,16 +47,25 @@ self.addEventListener('activate', (e) => {
 
 // ** On All Fetch Request, Intercept and Serve Through SW; If Offline, Serve Cached offline.html Page
 self.addEventListener('fetch', (e) => {
-  // console.log("Fetch Listener: ", e.request);
+  console.log("Fetch Listener Intercept: ", e.request);
+  console.log();
   e.respondWith(
     caches
       .match(e.request)
       .then(response => {
-        return fetch(e.request)
+        return (
+          // We can return just this fetch req. I added the .then() so I can see the log of the server response
+          fetch(e.request)
+            .then(serverResponse => {
+              console.log("Fetch Listener Server Response: ", serverResponse);
+                return serverResponse
+            })
+        )
+          
       })
       .catch(err => {
         console.log("On SW Fetch Intercept, response error: ", err);
         return caches.match('/assets/pwa-assets/offline.html')
-      })  
+      }) 
   )
 });
