1c4718c645ab86c47b1d9203a9182b89482bdc80
Diego Hernandez <ishtaracademy@outlook.com>
2020-12-15 01:04:30 -0700

    Moved and renamed manifest.json to pwa-assets/manifest.webmanifest; Created and setup service-worker.js for caching assets; created offline.html as a placeholder for showing a page when a user if offline

---
+++ client/public/assets/pwa-assets/service-worker.js
@@ -0,0 +1,53 @@
+const cacheFileName = "wc-cache-v1.6";
+const filesToCache = [
+  './offline.html'
+]
+
+
+self.addEventListener('install', (e) => {
+  console.log("Hit sw install");
+  e.waitUntil(
+    caches
+      .open(cacheFileName)
+      .then(cache => {
+        return cache.addAll(filesToCache)
+      })
+      .catch(err => console.log("On SW Install, caching error: ", err))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  console.log("hit sw activation");
+  e.waitUntil(
+    caches
+      .keys()
+      .then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== cacheFileName) {
+              console.log("Deleting Key: ", key);
+              return caches.delete(key);
+            }
+          })
+        )
+      })
+      .catch(err => console.log("On SW Activation, key delete error: ", err))
+  )
+})
+
+// self.ClientRectList.claim();
+
+// Sooooo, since 
+
+self.addEventListener('fetch', (e) => {
+  console.log("Fetch Listener: ", e.request);
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then(response => {
+        return response || fetch(event.request)
+      })
+      .catch(err => console.log("On SW Fetch Intercept, response error: ", err))
+  )
+});
