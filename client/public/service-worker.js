dac17dbd82f132e33e4fc9f36809b864cb31cee0
Diego Hernandez <ishtaracademy@outlook.com>
2020-12-15 01:49:18 -0700

    Added comments, commented some clogs

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,11 +1,14 @@
+// * Global Variables
 const cacheFileName = "wc-cache-v1.6";
 const filesToCache = [
   './assets/pwa-assets/offline.html'
 ]
 
 
+// * Event Listeners
+// ** On SW Install, Catch Files for Offline Use
 self.addEventListener('install', (e) => {
-  console.log("Hit sw install");
+  // console.log("Hit sw install");
   e.waitUntil(
     caches
       .open(cacheFileName)
@@ -15,10 +18,13 @@ self.addEventListener('install', (e) => {
       .catch(err => console.log("On SW Install, caching error: ", err))
   );
   self.skipWaiting();
+  // NOTE: I'm having an issue getting this to work, or I'm just doing it wrong 😅: service-worker.js:1 Uncaught (in promise) DOMException: Only the active worker can claim clients
+  // self.clients.claim();
 });
 
+// ** On SW Activation, Check For cacheFileName Change and Delete Old File
 self.addEventListener('activate', (e) => {
-  console.log("hit sw activation");
+  // console.log("hit sw activation");
   e.waitUntil(
     caches
       .keys()
@@ -36,12 +42,12 @@ self.addEventListener('activate', (e) => {
   )
 })
 
-// self.ClientRectList.claim();
 
-// Sooooo, since 
 
+
+// ** On All Fetch Request, Intercept and Serve Through SW; If Offline, Serve Cached offline.html Page
 self.addEventListener('fetch', (e) => {
-  console.log("Fetch Listener: ", e.request);
+  // console.log("Fetch Listener: ", e.request);
   e.respondWith(
     caches
       .match(e.request)
