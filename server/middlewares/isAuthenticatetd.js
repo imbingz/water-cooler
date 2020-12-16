512ff6bbc4b6224566e9409dea8565ee07562850
Bing Z <bingz@me.com>
2020-12-16 15:43:05 -0700

    installed bcrypt jwt n initial auth middleware

---
+++ server/middlewares/isAuthenticatetd.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../../keys');
+const db = require('../models');
+
+module.exports = (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization) {
+        //401 Not Authorized
+        return res.status(401).json({ error: 'you must be logged in' });
+    }
+    //Access the token (authorization === Bearer "given token string")
+    const token = authorization.replace('Bearer ', '');
+    //use jwt package to verify the token
+    jwt.verify(token, JWT_SECRET, (err, payload) => {
+        if (err) {
+            return res.status(401).json({ error: 'you must be logged in' });
+        }
+
+        const { _id } = payload;
+
+        db.User.findById(_id).then(userdata => {
+            req.user = userdata; //allow to access user info
+            // only go to next after find query
+            next();
+        });
+    });
+};
