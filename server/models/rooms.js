e19d487e1070a7694dc36750f0af868cec9c4447
dianalynshull <dianalynshull@gmail.com>
2020-12-16 19:01:48 -0700

    used the lint:fix script to resolve the expected 4 spaces but found 1 tab inded error

--- server/models/rooms.js
+++ server/models/rooms.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
 const RoomSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	publicID: {
-		type: String,
-		required: true
-	},
-	users: {
-		type: Array,
-		default: []
-	}
+    name: {
+        type: String,
+        required: true
+    },
+    publicID: {
+        type: String,
+        required: true
+    },
+    users: {
+        type: Array,
+        default: []
+    }
 });
 
 const Room = mongoose.model('Room', RoomSchema);
