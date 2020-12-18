58202f099afa2dacd1fe267b2f2958cac95f8bd9
Bing Z <bingz@me.com>
2020-12-17 18:15:52 -0700

    added dotenv config path

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/.env' });
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
