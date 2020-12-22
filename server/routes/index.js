afdaecb81c8c0372d1e5e1265a426cd5f3b05418
Bing Z <bingz@me.com>
2020-12-21 22:36:02 -0700

    fixed conflict n kept auth route for convenience

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-// const users = require('./user.route');
-const auth = require('./auth.route');
 
-//  api/user route
-// router.use('/api/user', users);
-router.use('/api/user', auth); // ===> parallell with users
-=======
+// const auth = require('./auth.route');
+// router.use('/api/user', auth); // ===> parallell with users
+
+
 const users = require('./user.routes');
 const rooms = require('./room.routes');
 const socialSpaces = require('./socialspace.routes');
@@ -14,6 +11,5 @@ const socialSpaces = require('./socialspace.routes');
 router.use('/api/user', users);
 router.use('/api/room', rooms);
 router.use('/api/socialspace', socialSpaces);
->>>>>>> master
 
 module.exports = router;
