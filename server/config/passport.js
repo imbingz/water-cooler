7b5318c5f3ec260bdea75fb12378fea44e637f3c
Bing Z <bingz@me.com>
2020-12-24 02:07:04 -0700

    reformated the file

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,44 +1,45 @@
 // Import packages and modules 
+const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require('bcryptjs');
 const db = require("../models");
 
 
+passport.use(
+    new LocalStrategy(
+        { 
+            usernameField: 'email' 
+        }, 
+        (email, password, done) => {
+            // Match user
+            db.User
+                .findOne({email: email})
+                .then(user => {
+                if (!user) {
+                return done(null, false, { message: 'That email is not registered' });
+                }
 
-module.exports = function(passport) {
-  passport.use(
-    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-      // Match user
-      db.User
-        .findOne({email: email})
-        .then(user => {
-        if (!user) {
-          return done(null, false, { message: 'That email is not registered' });
-        }
+                // Match password
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if (err) throw err;
+                    if (isMatch) {
+                        return done(null, user);
+                    } else {
+                        return done(null, false, { message: 'Password incorrect' });
+                    }
+                });
+            });
+        })
+    );
 
-        // Match password
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-          if (err) throw err;
-          if (isMatch) {
-            return done(null, user);
-          } else {
-            return done(null, false, { message: 'Password incorrect' });
-          }
-        });
-      });
-    })
-  );
-
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
+  passport.serializeUser((user, cb) => {
+        cb(null, user);
   });
 
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
+  passport.deserializeUser((user, cb) => {
+        cb(null, user);
   });
-};
+
 
 
 //Export the configured passport 
--- server/server.js
+++ server/server.js
@@ -2,17 +2,13 @@ require('dotenv').config({ path: __dirname + '/.env' });
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
-const session = require("express-session");
-const passport = require("./config/passport");
+const session = require('express-session');
+const passport = require('./config/passport');
 
 // const cors = require('cors');
 const app = express();
 
-// Passport Config
-require('./config/passport')(passport);
-
 require('./config/db')();
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,6 +17,13 @@ const PORT = process.env.PORT || 5000;
 // parsing middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Use express sessions 
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 app.use(routes);
@@ -32,15 +35,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-// Use express sessions 
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.listen(PORT, () => {
     console.log('app running on PORT: ' + PORT);
-    // console.log(process.env.MONGODB_URI);
-    // console.log(process.env.JWT_SECRET);
 });
