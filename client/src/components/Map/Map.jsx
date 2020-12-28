2e7d87f4295b53bb25bd2231211b4850a6bb107f
Bing Z <bingz@me.com>
2020-12-28 14:55:16 -0700

    restructured map, added exrra bg option, dynamic

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 import io from 'socket.io-client';
-import Player from '../../components/Player';
+import Player from '../Player';
 
 /******************************* ALEX */
 let socket;
 
-
+if (!socket) {
+    socket = io('http://localhost:8080', {
+        transports: ['websocket']
+    }); //this is the  client connection. it starts when client connects
+}
 /*********************************** */
 
-function Map({ tiles, tileset, size, activeTile, setTiles, bgTile }) {
+function Map({ tiles, tileset, tilesets, setTileset, tileHeight, tileWidth }) {
+
     /******************************* ALEX  */
     const [player, setPlayer] = React.useState(null);
     const [players, setPlayers] = React.useState({});
@@ -19,11 +26,8 @@ function Map({ tiles, tileset, size, activeTile, setTiles, bgTile }) {
     ***************/
     const [greeting, setGreeting] = React.useState('');
 
-    if (!socket) {
-        socket = io('/', {
-            transports: ['websocket']
-        }); //this is the  client connection. it starts when client connects
 
+    React.useEffect(() => {
         socket.on('connect', () => {
             console.log('hit connect');
             setPlayer({ id: socket.id, name: Math.random() });
@@ -33,6 +37,11 @@ function Map({ tiles, tileset, size, activeTile, setTiles, bgTile }) {
         socket.on('connect_error', err => {
             console.log(err);
         });
+
+
+        /********************************
+       state plyrs: {"VEf-RG4xQsUqPWgpAAAF":{"x":0,"y":0},"QONco2PVTxzwGBjsAAAL":{"x":0,"y":0},"BiKQtjhxPP6g2fCFAAAN":{"x":255,"y":100},"ILWM32u5CTcQ6WWIAAAP":{"x":204,"y":427},"xWvlzx5m98uL4TvjAAAX":{"x":292,"y":264},"YhN8HnUv51JGxliaAAAZ":{"x":407,"y":234}}
+        * *************************** */
 
         socket.on('state', (state) => {
             if (!state) { return; } console.log(state);
@@ -47,13 +56,8 @@ function Map({ tiles, tileset, size, activeTile, setTiles, bgTile }) {
             console.log(msg);
             setGreeting(msg);
         });
-    }
-    
-    
+    }, []);
 
-    /********************************
-     state plyrs: {"VEf-RG4xQsUqPWgpAAAF":{"x":0,"y":0},"QONco2PVTxzwGBjsAAAL":{"x":0,"y":0},"BiKQtjhxPP6g2fCFAAAN":{"x":255,"y":100},"ILWM32u5CTcQ6WWIAAAP":{"x":204,"y":427},"xWvlzx5m98uL4TvjAAAX":{"x":292,"y":264},"YhN8HnUv51JGxliaAAAZ":{"x":407,"y":234}}
-    * *************************** */
 
     React.useEffect(() => {
         if (player) {
@@ -63,47 +67,18 @@ function Map({ tiles, tileset, size, activeTile, setTiles, bgTile }) {
     }, [player]);
 
 
-
     /*********************************** */
 
-
-    //define dropTile for onClick event -
-
-    //clone matrix (see reason above)
-    function cloneMatrix(m) {
-        //create an empty array with given length of m
-        const clone = new Array(m.length);
-        // **** pre-increment i
-        for (let i = 0; i < m.length; ++i) {
-            //clone[i] = [ ...m[i] ]
-            clone[i] = m[i].slice(0);
-        }
-        return clone;
-    }
-
-    function dropTile({ x, y }) {
-        setTiles(prev => {
-            //use cloneMatrix func to clone the matrix of previouse state, alter tile @x/y to the active tile
-            const clone = cloneMatrix(prev);
-            //swap tile with the activeTile
-            const tile = {
-                ...clone[y][x],
-                v: activeTile
-            };
-            //actual swap
-            clone[y][x] = tile;
-            return clone;
-        });
-    }
 
     return (
         <div
             style={ {
-                boxSizing: 'border-box',
-                backgroundColor: 'white',
-                overflow: 'hidden',
-                width: size.width,
-                height: size.height
+                position: 'absolute',
+                border: '1px solid grey',
+                top: 0,
+                left: 0,
+                zIndex: 100,
+                backgroundColor: 'white'
             } }>
 
             {greeting && <h6> { greeting } </h6> }
@@ -125,54 +100,36 @@ function Map({ tiles, tileset, size, activeTile, setTiles, bgTile }) {
                 );
             }) }
 
-            {/* background layer  */ }
-            <div style={ { position: 'absolute', zIndex: 1 } }>
-                { tiles.map((row, y) => (
-                    <div style={ { display: 'flex' } } key={ y }>
-                        {row.map((tile, x) => (
-                            <div
-                                onClick={ () => {
-                                    dropTile({ x, y });
-                                } }
-                                key={ tile.id }
-                                style={ {
-                                    borderTop: '1px solid black',
-                                    borderRight: '1px solid black',
-                                    background: `url(/sprites/${tileset}.png)
-                                    -${bgTile.x}px -${bgTile.y}px no-repeat`,
-                                    width: 32,
-                                    height: 32
-                                } }
-                            />
-                        )) }
-                    </div>
-                )) }
+            <div style={ { display: 'flex', margin: '10px 50px' } }>
+                <div style={ { padding: 8 } }><span> Choose Room Settings </span></div>
+                <div style={ { width: 200, marginLeft: 8 } }>
+                    <Dropdown options={ tilesets } value={ tileset } onChange={ tileset => setTileset(tileset.value) } />
+                </div>
             </div>
-            {/* Forefront layer */ }
-            <div style={ { position: 'absolute', zIndex: 2 } }>
-                { tiles.map((row, y) => (
-                    <div style={ { display: 'flex' } } key={ y }>
-                        {row.map((tile, x) => (
-                            <div
-                                onClick={ () => {
-                                    dropTile({ x, y });
-                                } }
-                                key={ tile.id }
-                                style={ {
-                                    borderTop: '1px solid black',
-                                    borderRight: '1px solid black',
-                                    background: `url(/sprites/${tileset}.png)
-                                      -${tile.v.x}px -${tile.v.y}px no-repeat`,
-                                    width: 32,
-                                    height: 32
-                                } }
-                            />
-                        )) }
-                    </div>
-                )) }
-            </div>
+
+            {/* display tiles using map */ }
+            {tiles.map((row, y) => (
+                // each div is a row - inline style
+                <div style={ { display: 'flex' } } key={ y }>
+                    {row.map((tile, x) => (
+                        <div
+                            key={ tile.id }
+                            style={ {
+                                borderTop: '1px solid grey',
+                                borderRight: '1px solid grey',
+                                background: `url(/sprites/${tileset}.png)
+								-${x * 32}px -${y * 32}px
+								no-repeat`,
+                                width: tileWidth,
+                                height: tileHeight
+                            } }
+                        />
+                    )) }
+                </div>
+            )) }
         </div>
     );
+
 }
 
 export default Map;
