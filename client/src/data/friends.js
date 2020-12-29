c92ddd1108459ddfd877651b9a89b2657b598d0e
Bing Z <bingz@me.com>
2020-12-29 00:20:55 -0700

    added temp data for ui

---
+++ client/src/data/friends.js
@@ -0,0 +1,45 @@
+const friends = [
+    {
+        id: 1,
+        username: 'Bertieates',
+        firstName: 'Atheon',
+        lastName: 'Times-Conflux',
+        imageSrc:
+      'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
+    },
+    {
+        id: 2,
+        username: 'Hesterogan',
+        firstName: 'Crota',
+        lastName: 'Son-of-Oryx',
+        imageSrc:
+      'https://i.imgur.com/izGw0wB.jpg',
+    },
+    {
+        id: 3,
+        username: 'kingsfall',
+        firstName: 'Oryx',
+        lastName: 'The-Taken-King',
+        imageSrc:
+      'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
+    },
+    {
+        id: 4,
+        username: 'Seanalsh',
+        firstName: 'Oryx-Raid',
+        lastName: 'The-King',
+        imageSrc:  
+      'https://cdn130.picsart.com/246483773027202.jpg?type=webp&to=crop&r=256',
+    },
+    {
+        id: 5,
+        username: 'Lolaardner',
+        firstName: 'Aksis',
+        lastName: 'Archon-Prime',
+        imageSrc:
+      'https://pm1.narvii.com/6336/2169b35340c8109de7460b985874c375bf1dc244_128.jpg',
+    }
+];
+
+
+export default friends;
