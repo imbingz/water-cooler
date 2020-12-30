bf0d26111c18b467049e30becd3757cc142d66db
Bing Z <bingz@me.com>
2020-12-30 16:27:32 -0700

    iinitial grid layout

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,43 @@
 
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
 
 const Homepage = () => {
     return (
-        <>
-            <h1>This is the Homepage page</h1>
-        </>
+        <main>
+            <Container className='d-flex flex-column justify-content-end'>
+                <Row>
+                    <Col className='Home-room-header'>
+                        <h3>Create Room</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} lg={4}>
+                        <h3>carousel of Room Options</h3>
+                    </Col>
+                    <Col xs={12} md='auto'>
+                    
+                        <Row>
+                            room row
+                        </Row>
+                        <Row>
+                            friends row 
+                        </Row>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} lg={4}>
+                        <h3>Friends Room Card 1</h3>
+                    </Col>
+                    <Col xs={12} lg={4}>
+                        <h3>Friends Room Card 2</h3>
+                    </Col>
+                    <Col xs={12} lg={4}>
+                        <h3>Friends Room Card 3</h3>
+                    </Col>
+                </Row>
+            </Container>
+        </main>
     );
 };
 
