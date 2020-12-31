5841edb8a2623dcedd1556b4a173b924d71f0fac
Bing Z <bingz@me.com>
2020-12-30 19:52:50 -0700

    copy/paste more friends to temp db to test ui

--- client/src/data/friends.js
+++ client/src/data/friends.js
@@ -38,6 +38,46 @@ const friends = [
         lastName: 'Archon-Prime',
         imageSrc:
       'https://pm1.narvii.com/6336/2169b35340c8109de7460b985874c375bf1dc244_128.jpg',
+    },
+    {
+        id: 6,
+        username: 'Bertieates',
+        firstName: 'Atheon',
+        lastName: 'Times-Conflux',
+        imageSrc:
+      'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
+    },
+    {
+        id: 7,
+        username: 'Hesterogan',
+        firstName: 'Crota',
+        lastName: 'Son-of-Oryx',
+        imageSrc:
+      'https://i.imgur.com/izGw0wB.jpg',
+    },
+    {
+        id: 8,
+        username: 'kingsfall',
+        firstName: 'Oryx',
+        lastName: 'The-Taken-King',
+        imageSrc:
+      'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
+    },
+    {
+        id: 9,
+        username: 'Seanalsh',
+        firstName: 'Oryx-Raid',
+        lastName: 'The-King',
+        imageSrc:  
+      'https://cdn130.picsart.com/246483773027202.jpg?type=webp&to=crop&r=256',
+    },
+    {
+        id: 10,
+        username: 'Lolaardner',
+        firstName: 'Aksis',
+        lastName: 'Archon-Prime',
+        imageSrc:
+      'https://pm1.narvii.com/6336/2169b35340c8109de7460b985874c375bf1dc244_128.jpg',
     }
 ];
 
