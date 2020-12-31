fe59edf2deef114be8fd2b896a71610ac33e6109
Bing Z <bingz@me.com>
2020-12-30 22:26:07 -0700

    added room img size limitation

--- client/src/components/RoomCarousel/RoomCarousel.jsx
+++ client/src/components/RoomCarousel/RoomCarousel.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Carousel, Col} from 'react-bootstrap';
+import {Carousel} from 'react-bootstrap';
 import roomStyles from '../../data/roomStyles';
 import './RoomCarousel.css';
 
@@ -20,19 +20,20 @@ function RoomCarousel() {
 
             {roomStyles.map(style => {
                 return (
-                    <Col xs={12} lg={5} md={6} className='pb-3' >
-                        <Carousel.Item>
-                            <h5 className='text-secondary text-center pb-3'> Choose A Room Style </h5>
-                            <img
-                                className="d-block w-100"
-                                src={style.src}
-                                alt={style.title}
-                            />
+                   
+                    <Carousel.Item>
+                        <h5 className='text-secondary text-center pb-3'> Choose A Room Style </h5>
+                        <img
+                            className="d-block w-100"
+                            src={style.src}
+                            alt={style.title}
+                            style={{width: 288, height: 200}}
+                        />
                      
-                            <h6 className='text-secondary text-center pt-3' >{style.title}</h6>
+                        <h6 className='text-secondary text-center pt-3' >{style.title}</h6>
                      
-                        </Carousel.Item>
-                    </Col>
+                    </Carousel.Item>
+                  
                 );
             })}
         </Carousel>
