b32928a64ebf2373aae5b2dbd056dc70be0cf4b8
Bing Z <bingz@me.com>
2020-12-30 17:53:24 -0700

    create a room bg style data file

---
+++ client/src/data/roomStyles.js
@@ -0,0 +1,16 @@
+const roomStyles = [
+    {
+        src: '/sprites/rpg-nature-tileset/spring.png',
+        title: 'Nature Set - Spring'
+    },
+    {
+        src: '/sprites/rpg-nature-tileset/fall.png',
+        title: 'Nature Set - Fall'
+    },
+    {
+        src: '/sprites/rpg-nature-tileset/winter.png',
+        title: 'Nature Set - Winter'
+    },
+];
+
+export default roomStyles;
