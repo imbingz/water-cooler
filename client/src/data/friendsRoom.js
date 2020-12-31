0cd96288b8f3abe3c38e5d7dfc42a593c7fdc1f3
Bing Z <bingz@me.com>
2020-12-30 22:05:38 -0700

    created a freindsRoom seeds db

---
+++ client/src/data/friendsRoom.js
@@ -0,0 +1,60 @@
+const friendsRoom = [
+    {
+        roomname: 'Son-of-Oryx',
+        roomDiscription: 'dolor sit amet consectetur adipisicing elit. Sapiente',
+        roomStyle: '/sprites/rpg-nature-tileset/spring.png',
+        host: 'Atheon',
+        members: 8,
+        duration: '16min',
+    },
+    {
+        roomname: 'Times-Conflux',
+        roomDiscription: 'dolor sit amet consectetur adipisicing elit. Sapiente',
+        roomStyle: '/sprites/rpg-nature-tileset/fall.png',
+        host: 'Scrota',
+        members: 8,
+        duration: '42min',
+    },
+    {
+        roomname: 'The-Taken-King',
+        roomDiscription: 'dolor sit amet consectetur adipisicing elit. Sapiente',
+        roomStyle: '/sprites/rpg-nature-tileset/winter.png',
+        host: 'kingsfall',
+        members: 8,
+        duration: '34min',
+    },
+    {
+        roomname: 'Son-of-Oryx',
+        roomDiscription: 'dolor sit amet consectetur adipisicing elit. Sapiente',
+        roomStyle: '/sprites/rpg-nature-tileset/cafe.png',
+        host: 'Crota',
+        members: 8,
+        duration: '41min',
+    },
+    {
+        roomname: 'Oryx-Raid',
+        roomDiscription: 'dolor sit amet consectetur adipisicing elit. Sapiente',
+        roomStyle: '/sprites/rpg-nature-tileset/casino.png',
+        host: 'The-King',
+        members: 8,
+        duration: '19min',
+    },
+    {
+        roomname: 'Archon-Prime',
+        roomDiscription: 'dolor sit amet consectetur adipisicing elit. Sapiente',
+        roomStyle: '/sprites/rpg-nature-tileset/fall.png',
+        host: 'Spider',
+        members: 8,
+        duration: '32min',
+    },
+    {
+        roomname: 'kingsfall',
+        roomDiscription: 'dolor sit amet consectetur adipisicing elit. Sapiente',
+        roomStyle: '/sprites/rpg-nature-tileset/winter.png',
+        host: 'Auryx',
+        members: 8,
+        duration: '23min',
+    },
+];
+
+export default friendsRoom;
