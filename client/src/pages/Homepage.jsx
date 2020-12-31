63460af665a9cb31ff95e961866a6ef0df7fe85f
Bing Z <bingz@me.com>
2020-12-30 19:51:48 -0700

    createRoom component

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import RoomCarousel from '../components/RoomCarousel';
+import CreateRoom from '../components/CreateRoom';
 
 const Homepage = () => {
     return (
@@ -9,22 +10,17 @@ const Homepage = () => {
             <Container className='d-flex flex-column justify-content-end'>
                 <Container className='mt-3 p-4' style={{backgroundColor: 'greenyellow'}}> 
                     <Row >
-                        <Col className='text-center pb-2'>
+                        <Col className='text-center pb-2 mb-4'>
                             <h3>Create Room</h3>
                         </Col>
                     </Row>
-                    <Row className='d-flex justify-content-center  mx-5'>
-                        <Col xs={12} lg={5} md={6}>
+                    <Row className='d-flex justify-content-center mx-5'>
+                        <Col xs={12} lg={5} md={6} className='pb-3' >
                             <RoomCarousel />
                         </Col>
-                        <Col xs={12} lg={7} md={6}>
+                        <Col xs={12} lg={7} md={6} className='pl-2 pb-3'>
                     
-                            <Row>
-                            room row
-                            </Row>
-                            <Row>
-                            friends row 
-                            </Row>
+                            <CreateRoom/>
                         </Col>
                     </Row>
                 </Container>
