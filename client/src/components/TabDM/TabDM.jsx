d6ce1752b9dc159f87b4dc163daa3c782a323e13
Bing Z <bingz@me.com>
2021-01-01 23:20:18 -0700

    added 2nd chat side slider

--- client/src/components/TabDM/TabDM.jsx
+++ client/src/components/TabDM/TabDM.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import {AiOutlineClose} from 'react-icons/ai';
 import friends from '../../data/friends';
 import './TabDM.css';
 import {v4 as uuidv4} from 'uuid';
 
 function TabDM() {
+    const [sidebar, setSidebar] = useState(true);
+    const showSidebar = () => setSidebar(!sidebar);
+
+
     return (
         <section className='my-4 mx-3 TabDM-section'>
                 
@@ -12,11 +18,25 @@ function TabDM() {
                 <div className='d-flex flex-row justify-content-start' key={uuidv4()}>  
                     <img src={friend.imageSrc} alt={friend.username} style={{width:32, height: 32}}/>
                     <p className='mx-2' >{friend.username}</p>
-                    <button className='TabDM-view-chat-btn d-inline-block ml-auto mb-3 px-2 py-1'><small> View Chat</small> </button>
-                </div>
-                   
+                    <button 
+                        className='TabDM-view-chat-btn d-inline-block ml-auto mb-3 px-2 py-1' 
+                        onClick={showSidebar} 
+                    ><small> View Chat</small> </button>
+                </div>                   
             ))
             }
+
+            <aside className={sidebar ? 'Sidenav-menu active' : 'Sidenav-menu'}>
+                <div className='Sidenav-menu-items d-flex flex-column' >
+                    <div onClick={showSidebar}className='Sidenav-toggle' >
+                        <Link to='#' className='Header-menu-bars'>
+                            <AiOutlineClose />
+                        </Link>
+                    </div>
+                    {/* <Tabchat /> */}
+                </div>
+                
+            </aside>
         </section>
     );
 }
