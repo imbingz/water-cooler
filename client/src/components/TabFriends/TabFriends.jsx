e48e10cc001ec06aca00fc7b100f24aaaccd6fe3
Bing Z <bingz@me.com>
2021-01-01 22:32:21 -0700

    added scroll in  each section

--- client/src/components/TabFriends/TabFriends.jsx
+++ client/src/components/TabFriends/TabFriends.jsx
@@ -1,40 +1,34 @@
 import React from 'react';
 import './TabFriends.css';
-import {Button} from 'react-bootstrap';
 import friends from '../../data/friends';
 import friendsRoom from '../../data/friendsRoom';
 import {v4 as uuidv4} from 'uuid';
-// import {ImCheckboxChecked} from 'react-icons/im';
-// import {AiOutlineCloseCircle} from 'react-icons/ai';
 
 
 function TabFriends() {
     return (
         <div className='mx-3 mt-3'>
-            <section className='d-flex flex-column justify-content-start'>
-                <div className='d-flex justify-content-start'>
-                    <h6 className='mr-5'>Invitations:</h6> 
-                    <small className='ml-5 text-success '>Accept</small>  
-                    <small className='ml-4 text-danger'>Decline</small>  
-                </div>
+            <div className='d-flex justify-content-start'>
+                <h6 className='mr-5 Tabfriends-subtitle'>Invitations:</h6> 
+                <small className='ml-5 text-success '>Accept</small>  
+                <small className='ml-4 text-danger'>Decline</small>  
+            </div>
                 
-                 
+            <section className='d-flex flex-column justify-content-start TabFriends-section'>        
                 { friends &&
-                    friends.slice(0,3).map(friend => (
+                    friends.map(friend => (
                         <div className='d-flex flex-row justify-content-start' key={uuidv4()}>  
                             <img src={friend.imageSrc} alt={friend.username} style={{width:32, height: 32}}/>
                             <p className='mx-2' >friend request</p>
                             <input className='d-inline-block mx-5' style={{width: 18, height: 18}} type="radio" name={friend.id} value="accept" />
                             <input style={{width: 18, height: 18}} type="radio" name={friend.id} value="decline"/>
-                            {/* <button className='mx-5 TabFriends-check-btn' onClick={()=>console.log('accepted req')}><ImCheckboxChecked size={20} style={ { fill: 'blue' } }/></button>
-                            <button className='TabFriends-check-btn'><AiOutlineCloseCircle size={25} style={ { fill: 'red' } }/></button> */}
                         </div>
                            
                     ))
                 }
               
                 { friendsRoom &&
-                    friendsRoom.slice(0,2).map(friendRoom => (
+                    friendsRoom.map(friendRoom => (
                         <div className='d-flex flex-row justify-content-start' key={uuidv4()}>  
                             <img src={friendRoom.roomStyle} alt={friendRoom.roomname} style={{width:32, height: 32}}/>
                             <p className='ml-2 mr-4' >Room Invite</p>
@@ -47,10 +41,11 @@ function TabFriends() {
                 }
               
             </section>
-            <section className='mt-4 mr-3'>
-                <h6>Online Friends:</h6>
+            <div><h6 className='Tabfriends-subtitle mt-4'>Online Friends:</h6></div>
+            <section className='mb-4 mr-3 TabFriends-section'>
+                
                 { friends &&
-                    friends.slice(0,3).map(friend => (
+                    friends.map(friend => (
                         <div className='d-flex flex-row justify-content-start' key={uuidv4()}>  
                             <img src={friend.imageSrc} alt={friend.username} style={{width:32, height: 32}}/>
                             <p className='mx-2' >{friend.username}</p>
@@ -60,13 +55,18 @@ function TabFriends() {
                     ))
                 }
             </section>
-            <section>
-                <h6>offiline Friends:</h6>
-                <div className='d-flex flex-row justify-content-start'>    
-                    <p>Frnd img</p>
-                    <p>Frnd username</p>
-                    <button>View Profile</button> 
-                </div>
+            <div><h6 className='Tabfriends-subtitle mt-4'>offiline Friends:</h6></div>
+            <section className='mr-3 TabFriends-section'>    
+                { friends &&
+                    friends.map(friend => (
+                        <div className='d-flex flex-row justify-content-start' key={uuidv4()}>  
+                            <img src={friend.imageSrc} alt={friend.username} style={{width:32, height: 32}}/>
+                            <p className='mx-2' >{friend.username}</p>
+                            <button className='TabFriends-profile-btn d-inline-block ml-auto mb-3 px-2 py-1'><small> View Profile</small> </button>
+                        </div>
+                           
+                    ))
+                }
             </section>
         </div>
     );
