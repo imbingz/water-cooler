43607d53336894b597744fe13939c4515a53e53f
Bing Z <bingz@me.com>
2021-01-02 23:51:37 -0700

    changed mail icon, font size n color

--- client/src/components/ProfileModal/ProfileModal.jsx
+++ client/src/components/ProfileModal/ProfileModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Modal, Container, Row, Button, Col } from 'react-bootstrap';
-import {HiOutlineMail} from 'react-icons/hi';
+import {GoMail} from 'react-icons/go';
+
 function ProfileModal(props) {
     const {friend, onHide} = props;
     return (
@@ -46,37 +47,35 @@ function ProfileModal(props) {
 
             <Modal.Body>
                 
-                <section className='d-flex flex-column justify-content-center align-items-center mx-auto' style={{backgroundColor: '#ddc9fc'}}>
+                <Container className='d-flex flex-column justify-content-center align-items-center mx-auto' style={{backgroundColor: '#ddc9fc'}}>
                     <Row className='mt-5'>
                       
-                        <div className='mt-1'>
+                        <Col className='mt-1 mr-4' xs={3}>
                             <img src={friend.imageSrc} alt={friend.username} style={{width: 75, height: 75, borderRadius:50}}/>
-                        </div>
+                        </Col>
                      
-                        <Col className='ml-2'>
-                            <div className='d-flex flex-row justify-content-start'>
+                        <Col className='ml-2' xs='auto'>
+                            <Row className='d-flex flex-row justify-content-start'>
                                 <h6 className='pt-1'> {friend.firstName} </h6>      
                                 <h6 className='pt-1 mx-2'>  {friend.lastName} </h6> 
-                            </div>
-                            <p className='mb-0 Profile-Modal-username' style={{fontSize: '1rem', color: '#333'}}> {friend.username} </p>
-                            <div>
-                                <i className='mr-2'><HiOutlineMail size={22} style={ { fill: 'black' } }/></i>
-                                <a href = {`mailto: ${friend.email}`} style={{fontSize: '1rem', color: '#333'}}>{friend.email}</a> 
-                            </div>
-                            
-                            <Row>
-                                
                             </Row>
-                            <Row className='mb-3'>
-                                
-                            </Row>    
+                            <Row>
+                                <em className='mb-0 Profile-Modal-username' style={{fontSize: '1rem', color: '#555'}}> {friend.username} </em>
+                            </Row>
+                            <Row>
+                                <i className='mr-2'><GoMail size={20} style={ { fill: 'blue' } }/></i>
+                                <em>
+                                    <a href = {`mailto: ${friend.email}`} style={{fontSize: '1rem', color: '#555'}}>{friend.email}
+                                    </a>
+                                </em>
+                            </Row> 
                         </Col>
                     </Row>            
                     <Row className='my-4'>
                         <Button onClick={onHide} className='d-inline-block mx-2 px-3' variant='danger' size='sm'>Block</Button >
                         <Button onClick={onHide} className='d-inline-block mx-2 px-3' sizee='sm' variant='success'>Invite</Button >
                     </Row>
-                </section>           
+                </Container>           
             </Modal.Body>
         </Modal>
     );
