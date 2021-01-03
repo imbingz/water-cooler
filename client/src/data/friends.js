47ba5b65094ca4a91d4850728cc176260bc75b44
Bing Z <bingz@me.com>
2021-01-02 22:28:59 -0700

    added email in friends data for rendering test

--- client/src/data/friends.js
+++ client/src/data/friends.js
@@ -4,6 +4,7 @@ const friends = [
         username: 'Bertieates',
         firstName: 'Atheon',
         lastName: 'Times-Conflux',
+        email: 'atheon@cooler.io',
         imageSrc:
       'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
     },
@@ -12,6 +13,7 @@ const friends = [
         username: 'Hesterogan',
         firstName: 'Crota',
         lastName: 'Son-of-Oryx',
+        email: 'crota@cooler.io',
         imageSrc:
       'https://i.imgur.com/izGw0wB.jpg',
     },
@@ -20,6 +22,7 @@ const friends = [
         username: 'kingsfall',
         firstName: 'Oryx',
         lastName: 'The-Taken-King',
+        email: 'oryx@cooler.io',
         imageSrc:
       'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
     },
@@ -28,6 +31,7 @@ const friends = [
         username: 'Seanalsh',
         firstName: 'Oryx-Raid',
         lastName: 'The-King',
+        email: 'oryxraid@cooler.io',
         imageSrc:  
       'https://cdn130.picsart.com/246483773027202.jpg?type=webp&to=crop&r=256',
     },
@@ -36,6 +40,7 @@ const friends = [
         username: 'Lolaardner',
         firstName: 'Aksis',
         lastName: 'Archon-Prime',
+        email: 'aksis@cooler.io',
         imageSrc:
       'https://pm1.narvii.com/6336/2169b35340c8109de7460b985874c375bf1dc244_128.jpg',
     },
@@ -44,6 +49,7 @@ const friends = [
         username: 'Bertieates',
         firstName: 'Atheon',
         lastName: 'Times-Conflux',
+        email: 'conflux@cooler.io',
         imageSrc:
       'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
     },
@@ -52,6 +58,7 @@ const friends = [
         username: 'Hesterogan',
         firstName: 'Crota',
         lastName: 'Son-of-Oryx',
+        email: 'son_oryx@cooler.io',
         imageSrc:
       'https://i.imgur.com/izGw0wB.jpg',
     },
@@ -60,6 +67,7 @@ const friends = [
         username: 'kingsfall',
         firstName: 'Oryx',
         lastName: 'The-Taken-King',
+        email: 'takenking@cooler.io',
         imageSrc:
       'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
     },
@@ -68,6 +76,7 @@ const friends = [
         username: 'Seanalsh',
         firstName: 'Oryx-Raid',
         lastName: 'The-King',
+        email: 'theking@cooler.io',
         imageSrc:  
       'https://cdn130.picsart.com/246483773027202.jpg?type=webp&to=crop&r=256',
     },
@@ -76,6 +85,7 @@ const friends = [
         username: 'Lolaardner',
         firstName: 'Aksis',
         lastName: 'Archon-Prime',
+        email: 'archon@cooler.io',
         imageSrc:
       'https://pm1.narvii.com/6336/2169b35340c8109de7460b985874c375bf1dc244_128.jpg',
     }
