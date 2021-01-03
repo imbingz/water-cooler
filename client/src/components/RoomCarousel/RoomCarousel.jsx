e0a0a61d0d0424a8297790d458fc89a5e152ff62
Bing Z <bingz@me.com>
2021-01-03 00:53:12 -0700

    moved Col back from homepage

--- client/src/components/RoomCarousel/RoomCarousel.jsx
+++ client/src/components/RoomCarousel/RoomCarousel.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
-import {Carousel} from 'react-bootstrap';
+import {Carousel, Col} from 'react-bootstrap';
 import roomStyles from '../../data/roomStyles';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import './RoomCarousel.css';
 
 
@@ -17,25 +17,25 @@ function RoomCarousel() {
   
 
     return (
-   
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-            {
-                roomStyles.map((style, i) => (
-                    <Carousel.Item key={i}>
-                        <h3 className='text-center text-muted'>Choose A Room Style</h3>
-                        <img
-                            className="d-block w-100"
-                            src={style.src}
-                            alt={style.title}
-                        />
-                        {/* <Carousel.Caption> */}
-                        <h4 className='text-center text-muted'>{style.title}</h4>
-                        {/* </Carousel.Caption> */}
-                    </Carousel.Item>
-                ))
-            }
-        </Carousel>
-   
+        <Col xs={12} lg={5} md={6} className='pb-3' >   
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                {
+                    roomStyles.map((style, i) => (
+                        <Carousel.Item key={i}>
+                            <h3 className='text-center text-muted'>Choose A Room Style</h3>
+                            <img
+                                className="d-block w-100"
+                                src={style.src}
+                                alt={style.title}
+                            />
+                            {/* <Carousel.Caption> */}
+                            <h4 className='text-center text-muted'>{style.title}</h4>
+                            {/* </Carousel.Caption> */}
+                        </Carousel.Item>
+                    ))
+                }
+            </Carousel>
+        </Col>
     );
 }
 
