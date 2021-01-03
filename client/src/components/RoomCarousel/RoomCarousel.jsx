46dcd51e40b3de7c8a84d6f0fece924a037115ea
Bing Z <bingz@me.com>
2021-01-03 00:51:55 -0700

    uuid as key causing carousel issue. use index now

--- client/src/components/RoomCarousel/RoomCarousel.jsx
+++ client/src/components/RoomCarousel/RoomCarousel.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
-import {Carousel, Col} from 'react-bootstrap';
+import {Carousel} from 'react-bootstrap';
 import roomStyles from '../../data/roomStyles';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import './RoomCarousel.css';
 
 
@@ -14,33 +14,28 @@ function RoomCarousel() {
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
+  
 
     return (
-
-        <Col xs={12} lg={5} md={6} className='pb-3' >   
-            <Carousel activeIndex={index} onSelect={handleSelect}>
-
-
-                {roomStyles.map(style => {
-                    return (
-
-                        <Carousel.Item key={uuidv4()}>
-                            <h5 className='text-secondary text-center pb-3'> Choose A Room Style </h5>
-                            <img
-                                className="d-block w-100"
-                                src={style.src}
-                                alt={style.title}
-                                style={{width: 288, height: 200}}
-                            />
-                     
-                            <h6 className='text-secondary text-center pt-3' >{style.title}</h6>
-                     
-                        </Carousel.Item>
-                  
-                    );
-                })}
-            </Carousel>
-        </Col>
+   
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            {
+                roomStyles.map((style, i) => (
+                    <Carousel.Item key={i}>
+                        <h3 className='text-center text-muted'>Choose A Room Style</h3>
+                        <img
+                            className="d-block w-100"
+                            src={style.src}
+                            alt={style.title}
+                        />
+                        {/* <Carousel.Caption> */}
+                        <h4 className='text-center text-muted'>{style.title}</h4>
+                        {/* </Carousel.Caption> */}
+                    </Carousel.Item>
+                ))
+            }
+        </Carousel>
+   
     );
 }
 
