1e9e557d084ba85e72f4141e75dd6231aad7357c
Bing Z <bingz@me.com>
2021-01-05 15:23:01 -0700

    changed user id to label for ui render

--- client/src/data/dmchats.js
+++ client/src/data/dmchats.js
@@ -2,35 +2,35 @@
 const dmChats = [
     
     {  
-        user_id: '5fd91211f572e58bb516a3a7',
+        label:'friend',
         username: 'eclarke',        
         msg: 'let\'s do this!',
         timeStamp: new Date().toLocaleTimeString(),
         read: false
     },
     {  
-        user_id: '5fd91211f572e58bb516a3a7',
+        label:'you',
         username: 'the-king',        
         msg: 'can\'t wait!',
         timeStamp: new Date().toLocaleTimeString(),
         read: false
     },
     {  
-        user_id: '5fd91211f572e58bb516a3a7',
+        label:'friend',
         username: 'eclarke',        
         msg: 'when shall we start?',
         timeStamp: new Date().toLocaleTimeString(),
         read: false
     },
     {  
-        user_id: '5fd91211f572e58bb516a3a7',
+        label:'friend',
         username: 'eclarke',        
         msg: 'we shall start NOW!',
         timeStamp: new Date().toLocaleTimeString(),
         read: false
     },
     {  
-        user_id: '5fd91211f572e58bb516a3a7',
+        label:'you',
         username: 'the-king',        
         msg: 'I\'m ready!!!!!',
         timeStamp: new Date().toLocaleTimeString(),
