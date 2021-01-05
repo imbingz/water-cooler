ac0eee9bff74c41de9b0ceb7d7d4400e9dbca5ff
Bing Z <bingz@me.com>
2021-01-04 21:05:59 -0700

    added profile preset avatars seed data

---
+++ client/src/data/profileImages.js
@@ -0,0 +1,66 @@
+const profileImages = [
+    {
+        id: 1,
+        imageSrc:
+      'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
+    },
+    {
+        id: 2,
+        imageSrc:
+      'https://i.imgur.com/izGw0wB.jpg',
+    },
+    {
+        id: 3,
+        imageSrc:
+      'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
+    },
+    {
+        id: 4,
+        imageSrc:  
+      'https://cdn130.picsart.com/246483773027202.jpg?type=webp&to=crop&r=256',
+    },
+    {
+        id: 5,
+        imageSrc:
+      'https://pm1.narvii.com/6336/2169b35340c8109de7460b985874c375bf1dc244_128.jpg',
+    },
+    {
+        id: 6,
+        imageSrc:
+      'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
+    },
+    {
+        id: 7,
+        imageSrc:
+      'https://i.imgur.com/izGw0wB.jpg',
+    },
+    {
+        id: 8,
+        imageSrc:
+      'https://i.pinimg.com/originals/6a/78/f7/6a78f73a95511beb19cb7da69267e956.jpg',
+    },
+    {
+        id: 9,
+        imageSrc:  
+      'https://cdn130.picsart.com/246483773027202.jpg?type=webp&to=crop&r=256',
+    },
+    {
+        id: 10,
+        imageSrc:
+      'https://pm1.narvii.com/6336/2169b35340c8109de7460b985874c375bf1dc244_128.jpg',
+    }
+    ,
+    {
+        id: 11,
+        imageSrc:  
+      'https://cdn130.picsart.com/246483773027202.jpg?type=webp&to=crop&r=256',
+    },
+    {
+        id: 12,
+        imageSrc:
+      'https://pm1.narvii.com/6336/2169b35340c8109de7460b985874c375bf1dc244_128.jpg',
+    }
+];
+
+
+export default profileImages;
