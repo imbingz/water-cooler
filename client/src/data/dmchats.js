7bf30025650662b0193dd84b2b8189125b02548d
Bing Z <bingz@me.com>
2021-01-05 13:02:31 -0700

    changed timestamp to time only

--- client/src/data/dmchats.js
+++ client/src/data/dmchats.js
@@ -5,35 +5,35 @@ const dmChats = [
         user_id: '5fd91211f572e58bb516a3a7',
         username: 'eclarke',        
         msg: 'let\'s do this!',
-        timeStamp: new Date(Date.now()).toUTCString(),
+        timeStamp: new Date().toLocaleTimeString(),
         read: false
     },
     {  
         user_id: '5fd91211f572e58bb516a3a7',
         username: 'the-king',        
         msg: 'can\'t wait!',
-        timeStamp: new Date(Date.now()).toUTCString(),
+        timeStamp: new Date().toLocaleTimeString(),
         read: false
     },
     {  
         user_id: '5fd91211f572e58bb516a3a7',
         username: 'eclarke',        
         msg: 'when shall we start?',
-        timeStamp: new Date(Date.now()).toUTCString(),
+        timeStamp: new Date().toLocaleTimeString(),
         read: false
     },
     {  
         user_id: '5fd91211f572e58bb516a3a7',
         username: 'eclarke',        
         msg: 'we shall start NOW!',
-        timeStamp: new Date(Date.now()).toUTCString(),
+        timeStamp: new Date().toLocaleTimeString(),
         read: false
     },
     {  
         user_id: '5fd91211f572e58bb516a3a7',
         username: 'the-king',        
         msg: 'I\'m ready!!!!!',
-        timeStamp: new Date(Date.now()).toUTCString(),
+        timeStamp: new Date().toLocaleTimeString(),
         read: false
     },
 
