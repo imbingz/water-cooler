aa4c5e18b811f9a20bb988feac4c23d3783bdac4
Bing Z <bingz@me.com>
2021-01-05 19:21:49 -0700

    put slider into its own component

---
+++ client/src/components/Slider/Slider.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {useGlobalContext} from '../../utils/GlobalContext.js';
+import Tabnav from '../Tabnav';
+import {Col} from 'react-bootstrap';
+import {AiFillCloseCircle} from 'react-icons/ai';
+import './Slider.css';
+
+function Slider() {
+
+    const [{showAside}, dispatch] = useGlobalContext();
+    return (
+        <>
+            {showAside && <Col md='auto' className='mx-0 px-0 '>
+                <aside className={showAside ? 'Sidenav-menu active' : 'Sidenav-menu'}>
+                    <div className='Sidenav-menu-items d-flex flex-column' >
+                        <div onClick={()=>dispatch({type: 'setShowAside', payload: false})}className='Sidenav-toggle' >
+                            <Link to='#' className='Header-menu-bars'>
+                                <AiFillCloseCircle style={{fill: 'greenyellow'}}/>
+                            </Link>
+                        </div>
+                        <Tabnav />
+                    </div>
+                </aside>
+            </Col>}
+        </>
+    );
+}
+
+export default Slider;
