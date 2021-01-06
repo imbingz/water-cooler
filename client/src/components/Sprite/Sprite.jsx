3216830d9548b26b58264d19ec2fdad2f5817820
Bing Z <bingz@me.com>
2021-01-06 12:50:58 -0700

    cleaned up Sprite n re-positioned greeting msg

--- client/src/components/Sprite/Sprite.jsx
+++ client/src/components/Sprite/Sprite.jsx
@@ -14,29 +14,35 @@ function Sprite({position={x:0, y:0}, message, step=0, dir=0}) {
                     height: '32px',
                     width: '32px',
                     backgroundImage: 'url(\'/sprites/skins/m2.png\')',
-                    backgroundPosition: `-${step*32}px -${step*32}px`, 
+                    // backgroundPosition: ` -${x}px -${y}px`, 
+                    backgroundPosition: `-${step*32}px -${step*32}px`,
                     backgroundRepeat: 'no-repeat',
-                    zIndex: 10
+                    zIndex: 10,
                 } }
-            />
+            >
+                
+                <div
+                    style={ {
+                        padding: '0px 30px',
+                        zIndex: 10
+                    } }    
+                >
+                    userName
+                 
+                </div>
 
-            <div
-                style={ {
-                    padding: '0px 30px',
-                    zIndex: 10
-                } }    
-            >
-            userName
-            </div>
-            <div
-                style={ {
-                    position: 'absolute',
-                    top: position.y + '20px',
-                    left: position.x,
-                    zIndex: 10
-                } }    
-            >
-                <h6>{message}</h6>
+                <div
+                    style={ {
+                        position: 'absolute',
+                        top: position.y + '20px',
+                        left: position.x,
+                        // padding: '0px 30px',
+                        zIndex: 10
+                    } }    
+                >
+                    <h6>{message}</h6>
+                </div>
+               
             </div>
         </>
     );
