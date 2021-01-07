f8ae16b3f087f463b32458ae06317caa85aa7799
Bing Z <bingz@me.com>
2021-01-06 23:44:23 -0700

    added roomChats db for TabRoomChats render

---
+++ client/src/data/roomChats.js
@@ -0,0 +1,41 @@
+
+const roomChats = [
+    
+    {  
+      
+        username: 'eclarke',        
+        msg: 'Hi all, eclarke here',
+        timeStamp: new Date().toLocaleTimeString(),
+        read: false
+    },
+    {  
+       
+        username: 'the-king',        
+        msg: 'the-king here',
+        timeStamp: new Date().toLocaleTimeString(),
+        read: false
+    },
+    {  
+        username: 'oryx',        
+        msg: 'whats appp ya\'ll',
+        timeStamp: new Date().toLocaleTimeString(),
+        read: false
+    },
+    {  
+        username: 'eclarke',        
+        msg: 'game night tonight?',
+        timeStamp: new Date().toLocaleTimeString(),
+        read: false
+    },
+    {  
+        username: 'the-king',        
+        msg: 'I\'m ready!!!!!',
+        timeStamp: new Date().toLocaleTimeString(),
+        read: false
+    },
+
+
+   
+];
+
+export default roomChats;
