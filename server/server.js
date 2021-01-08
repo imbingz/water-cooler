a3f413d51d42d88f1b983cae6cfd08100fa5368e
dianalynshull <dianalynshull@gmail.com>
2021-01-07 22:57:58 -0700

    added cors back into server. Commented out all current server sockets other than disconnect

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ const passport = require('./config/passport');
 const app = express();
 
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '/:3000',
+        methods: ['GET', 'POST']
+    }
+});
 
 require('./config/db')();
 
@@ -34,92 +39,92 @@ app.use(passport.session());
 app.use(routes);
 
 //******************************** Alex
-const players = {};
+// const players = {};
 
 io.on('connect', (socket) => {
 //******************************** Bing
-    players[socket.id] = {
-        x: 0, y: 0
-    };
-    io.sockets.emit('state', {players});
+    // players[socket.id] = {
+    //     x: 0, y: 0
+    // };
+    // io.sockets.emit('state', {players});
     //********************************//
-    socket.on('new-user', (roomUrlId, name) => {
-        socket.join(roomUrlId);
-        socket.room = roomUrlId;
-        socket.to(socket.room).broadcast.emit('user-connected', roomUrlId, name);
-    });
+    // socket.on('new-user', (roomUrlId, name) => {
+    //     socket.join(roomUrlId);
+    //     socket.room = roomUrlId;
+    //     socket.to(socket.room).broadcast.emit('user-connected', roomUrlId, name);
+    // });
 
-    socket.on('send-chat-message', (roomUrlId, name, messageInput) => {
-        socket.to(socket.room).emit('receive-sent-message', roomUrlId, name, messageInput);
-    });
+    // socket.on('send-chat-message', (roomUrlId, name, messageInput) => {
+    //     socket.to(socket.room).emit('receive-sent-message', roomUrlId, name, messageInput);
+    // });
 
-    socket.on('check-room', (roomUrlId, name) => {
-        if (roomUrlId === socket.room) {
-            return;
-        } 
-        socket.leave(socket.room);
-        socket.join(roomUrlId);
-        socket.room = roomUrlId;
-        socket.to(socket.room).broadcast.emit('user-connected', roomUrlId, name);
+    // socket.on('check-room', (roomUrlId, name) => {
+    //     if (roomUrlId === socket.room) {
+    //         return;
+    //     } 
+    //     socket.leave(socket.room);
+    //     socket.join(roomUrlId);
+    //     socket.room = roomUrlId;
+    //     socket.to(socket.room).broadcast.emit('user-connected', roomUrlId, name);
         
-    });
+    // });
 
     socket.on('disconnect', () => {
         socket.to(socket.room).broadcast.emit('user-disconnected');
         
         //******************************** Bing
-        delete players[socket.id];
+        // delete players[socket.id];
     });
 
 
    
     //******************************** Alex -  Bing
-    socket.on('movement', (data) => {
+    // socket.on('movement', (data) => {
     
-        console.log('movement called', {data});
-        // console.log({players});
-        if (data.x < 0) {
-            data.x = 0;
-        }
+    //     console.log('movement called', {data});
+    //     // console.log({players});
+    //     if (data.x < 0) {
+    //         data.x = 0;
+    //     }
 
-        if (data.x > 800) {
-            data.x = 800;
-        }
+    //     if (data.x > 800) {
+    //         data.x = 800;
+    //     }
 
-        if (data.y < 0) {
-            data.y = 0;
-        }
+    //     if (data.y < 0) {
+    //         data.y = 0;
+    //     }
 
-        if (data.y > 600) {
-            data.y = 600;
-        }
+    //     if (data.y > 600) {
+    //         data.y = 600;
+    //     }
 
-        players[socket.id] = data;
+    //     players[socket.id] = data;
 
-        let message; 
+    //     let message; 
 
-        if(players) {
-            console.log(players);
-            let playerPositions = Object.values(players);
-            for (let i = 0; !message && i < playerPositions.length ; i++) {
-                for (let j = i + 1; !message && j < playerPositions.length; j++) {
-                    // console.log(playerPositions[i].x)
-                    if (Math.abs( playerPositions[i].x - playerPositions[j].x ) <= 32 && Math.abs(playerPositions[i].y - playerPositions[j].y) <= 32) {
-                        console.log('distance x: ' + Math.abs(playerPositions[i].x - playerPositions[j].x) + ' distance y: ' + Math.abs(playerPositions[i].y - playerPositions[j].y));
-                        playerPositions[i].message = 'Hey, like to chat?'; 
-                        playerPositions[j].message = 'Hellllloooooo , genius'; 
+    //     if(players) {
+    //         console.log(players);
+    //         let playerPositions = Object.values(players);
+    //         for (let i = 0; !message && i < playerPositions.length ; i++) {
+    //             for (let j = i + 1; !message && j < playerPositions.length; j++) {
+    //                 // console.log(playerPositions[i].x)
+    //                 if (Math.abs( playerPositions[i].x - playerPositions[j].x ) <= 32 && Math.abs(playerPositions[i].y - playerPositions[j].y) <= 32) {
+    //                     console.log('distance x: ' + Math.abs(playerPositions[i].x - playerPositions[j].x) + ' distance y: ' + Math.abs(playerPositions[i].y - playerPositions[j].y));
+    //                     playerPositions[i].message = 'Hey, like to chat?'; 
+    //                     playerPositions[j].message = 'Hellllloooooo , genius'; 
                      
-                        // socket.emit('greeting', 'Hey, like to chat?');
-                    } 
-                }
-            }
-        }
+    //                     // socket.emit('greeting', 'Hey, like to chat?');
+    //                 } 
+    //             }
+    //         }
+    //     }
         
-        // players[socket.id] = data;
+    //     // players[socket.id] = data;
 
-        socket.emit('state', {players, message});
+    //     socket.emit('state', {players, message});
         
-    });
+    // });
     /*********************************** */
 });
 
