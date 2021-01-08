b1f5930489885e5712876224c81dae169703a2f5
Bing Z <bingz@me.com>
2021-01-07 20:34:37 -0700

    logout n btn

--- client/src/pages/Space.jsx
+++ client/src/pages/Space.jsx
@@ -1,40 +1,22 @@
 import React, {useState} from 'react';
 import {Container} from 'react-bootstrap';
-import {MdExpandMore} from 'react-icons/md';
-import './Space.css';
-import {RiMoreLine} from 'react-icons/ri';
+
 
 function Space() {
-
-    const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar);
-
-
     return (
-        <Container className='d-flex flex-row'>
-            <section style={{background:'pink'}} className='Space-video-section'>
-                <div className='d-flex justify-content-end'>
-                    <button className='Sidenav-header-icon-btn'>
-                        <RiMoreLine size={30} style={{fill: 'grey'}} onClick={showSidebar}/>
-                    </button>
-                </div>
-                <div style={{background:'beige'}}>
-                    vidoes
+        <Container>
+            <section style={{backgroundColor:'lightgreen'}} className='Space-video-section mb-5'>
+                <div style={{backgroundColor:'lightyellow'}} className='d-flex justify-content-center flex-wrap'>
+                    <div className='p-3'>
+                        <h6 className='text-muted'>The-King-Oryx</h6>
+                        <video autoplay playsinline src='/assets/demo/temp-video.mp4' style={{width: 500}}></video>      
+                    </div>
+                    <div className='p-3'>
+                        <h6 className='text-muted'>The-King-Oryx</h6>
+                        <video autoplay playsinline src='/assets/demo/temp-video.mp4' style={{width: 500}}></video>      
+                    </div>
                 </div>
             </section>
-
-            <aside className={sidebar ? 'Sidenav-menu-space active' : 'Sidenav-menu-space'}>
-                <div className='Sidenav-menu-items-space' >
-                    <div className='Sidenav-toggle-space' >
-                        <div className='d-flex justify-content-end'>
-                            <button className='Sidenav-header-icon-btn'>
-                                <MdExpandMore size={30} style={{fill: 'orangered'}} onClick={showSidebar}/>
-                            </button>
-                        </div>
-                        
-                    </div>
-                </div>          
-            </aside>
         </Container>
     );
 }
