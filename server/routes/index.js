03cad19e065f2802e94077059396c9135e1a63c4
dianalynshull <dianalynshull@gmail.com>
2021-01-07 23:04:20 -0700

    removed the socket route from route index and deleted the socket routes file

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,14 +10,11 @@ router.use('/api/user', auth); // ===> authenticated parallel with users
 const friends = require('./friends.routes');
 const rooms = require('./room.routes');
 const socialSpaces = require('./socialspace.routes');
-const ioRoutes = require('./socket.routes');
 
 router. use('/api/friends', friends);
 // api/room route
 router.use('/api/room', rooms);
 // api/social space route
 router.use('/api/socialspace', socialSpaces);
-// api/socket io  route
-router.use('/api/socketio', ioRoutes);
 
 module.exports = router;
