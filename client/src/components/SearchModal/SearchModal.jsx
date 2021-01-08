6ab544af76f7d632640ba1051c36e19954c85fc5
Bing Z <bingz@me.com>
2021-01-08 16:43:07 -0700

    added toastify in seach modal

--- client/src/components/SearchModal/SearchModal.jsx
+++ client/src/components/SearchModal/SearchModal.jsx
@@ -3,6 +3,7 @@ import SearchResults from '../SearchResults';
 import SearchContext from '../../utils/SearchContext';
 import { Modal, Container } from 'react-bootstrap';
 import { BsSearch } from 'react-icons/bs';
+import { toast } from 'react-toastify';
 import './SearchModal.css';
 
 function SearchModal (props) {
@@ -22,7 +23,10 @@ function SearchModal (props) {
             });
             const data = await response.json();
             if (!data.success) {
-                window.alert('No match 😮');
+                // window.alert('No match 😮');
+                toast.error('No match 😮', {
+                    position: toast.POSITION.TOP_CENTER
+                });
                 return;
             }
             // ** If Results Are Found, set State To Trigger SearchResults.jsx
