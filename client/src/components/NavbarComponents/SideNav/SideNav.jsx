c6f91c0275d85c44ea7ce35a1e6a0d0ebe56fa7b
Diego Hernandez <ishtaracademy@outlook.com>
2021-01-08 19:53:10 -0700

    expermenting with with navbar so we don't need two for landing and homepage; removed searchbar

--- client/src/components/NavbarComponents/SideNav/SideNav.jsx
+++ client/src/components/NavbarComponents/SideNav/SideNav.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BsPeopleCircle } from 'react-icons/bs';
+// import { BsPeopleCircle } from 'react-icons/bs';
 import { CgMenu } from 'react-icons/cg';
 import { IconContext } from 'react-icons';
 import { Link, useHistory } from 'react-router-dom';
-import NavSearch from '../NavSearch/NavSearch';
+import { Navbar } from 'react-bootstrap';
+// import NavSearch from '../NavSearch/NavSearch';
 import { useGlobalContext } from '../../../utils/GlobalContext';
 import './SideNav.css';
 
 function SideNav() {
 
     // Slider setup
-    const [{showAside}, dispatch] = useGlobalContext();
-    const showSidebar = () => dispatch({type: 'setShowAside', payload: !showAside});
+    const [{ showAside }, dispatch] = useGlobalContext();
+    const showSidebar = () => dispatch({ type: 'setShowAside', payload: !showAside });
 
     //Logout logic
     const history = useHistory();
@@ -37,20 +38,32 @@ function SideNav() {
     return (
         <header>
             <IconContext.Provider value={{ color: '#fff' }}>
-                <nav className='Header-navbar'>
+                <Navbar sticky='top'
+                    className="justify-content-between align-items-top bg-secondary"
+                >
                     <div className='Header-left'>
-                        <Link to='/profile' className='Header-profile-icon'>
+                        {/* <Link to='/profile' className='Header-profile-icon'>
                             <BsPeopleCircle />
-                        </Link>
+                        </Link> */}
                         <Link to='#' className='Header-menu-bars'>
                             <CgMenu onClick={showSidebar} />
                         </Link>
                     </div>
-                    <NavSearch />
-                    <div className='Header-right'>
-                        <button className='Header-logout-btn'onClick={() => handleLogout()}>Logout</button>
+                    {/* <NavSearch /> */}
+                    <div>
+                        <img style={{ width: 40, height: 40 }} src="/assets/images/logo-50.png" alt="water cooler logo" />
                     </div>
-                </nav>
+                    <h2 style={{ fontFamily: 'Great Vibes' }} className='mb-0 pt-2'>Water Cooler</h2>
+
+                    <a style={{ fontFamily: 'Great Vibes' }} className='mb-0 pt-2' href="/">
+                        <img style={{ width: 40, height: 40 }} src="/assets/images/logo-50.png" alt="water cooler logo" />
+                        Water Cooler
+                    </a>
+
+                    <div className='Header-right'>
+                        <button className='Header-logout-btn' onClick={() => handleLogout()}>Logout</button>
+                    </div>
+                </Navbar>
             </IconContext.Provider>
         </header>
     );
