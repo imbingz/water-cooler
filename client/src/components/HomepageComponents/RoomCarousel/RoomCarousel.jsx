d76d6ea87e0bd096944378e2e675398ed6660458
dianalynshull <dianalynshull@gmail.com>
2021-01-09 21:58:42 -0700

    removed empty spaces and removed console logs of the room carousel

--- client/src/components/HomepageComponents/RoomCarousel/RoomCarousel.jsx
+++ client/src/components/HomepageComponents/RoomCarousel/RoomCarousel.jsx
@@ -6,32 +6,21 @@ import { useGlobalContext } from '../../../utils/GlobalContext';
 // import { v4 as uuidv4 } from 'uuid';
 import './RoomCarousel.css';
 
-
-
-
 function RoomCarousel() {
    
     const [index, setIndex] = useState(0);
-
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
-    };
-  
+    }; 
     const [{roomStyle}, dispatch] = useGlobalContext();
-
     const handleSetRoomStyle = (style) => {
         console.log('img selected', style);
-
         dispatch({type: 'setRoomStyle', payload: style.variants });
-
         toast.dark(` 🥳 You selected ${style.title} style!!`, {
             position: toast.POSITION.TOP_CENTER
         });
-
     };
-
-    console.log('roomStyle', roomStyle);
-
+    
     return (
         <Col xs={12} lg={5} md={6} className='pb-3' >   
             <Carousel activeIndex={index} onSelect={handleSelect}>
