e9176efbd808e672299c331eb068d5e5965e45b5
Bing Z <bingz@me.com>
2021-01-12 11:39:01 -0700

    del unused var bycrpt

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const bcrypt = require('bcryptjs');
 const passport = require('../config/passport');
 const db = require('../models');
 const authRequired = require('../middlewares/authRequired');
