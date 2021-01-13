12b699da36e5adb631e2127e527371ecc627222e
Bing Z <bingz@me.com>
2021-01-12 19:52:49 -0700

    initial testing for jest itself

---
+++ server/_tests_/initial.test.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const supertest = require('supertest');
+const request = supertest(app);
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get('/test', async (req, res) => {
+    res.json({ message: 'pass!' });
+});
+  
+
+it('Testing to see if Jest works', () => {
+    expect(1).toBe(1);
+});
+  
+it('gets the test endpoint', async done => {
+    const response = await request.get('/test');
+  
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('pass!');
+    done();
+});
