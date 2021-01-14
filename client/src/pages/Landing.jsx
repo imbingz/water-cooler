a9d5e47f0391198967b643044fc8a61e5a07a4e6
Bing Z <bingz@me.com>
2021-01-13 21:23:59 -0700

    updated media path

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -21,7 +21,7 @@ function Landing() {
                 <Row className='d-flex justify-content-center mx-2 my-4'>             
                     <h3 className='text-center text-muted py-3'>  A Space To Bound & Connect </h3>
                       
-                    <img src="/assets/demo/landing.gif" alt="demo" style={{width:'85%', maxWidth: 950}}/>               
+                    <img src="/assets/images/landingimg/landing.gif" alt="demo" style={{width:'85%', maxWidth: 950}}/>               
                 </Row> 
                 
                 
