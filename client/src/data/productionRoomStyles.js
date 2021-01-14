2b321c58667596a29c7655849676fc0610770c48
Bing Z <bingz@me.com>
2021-01-14 14:40:09 -0700

    changed name to Production grade :)

---
+++ client/src/data/productionRoomStyles.js
@@ -0,0 +1,15 @@
+const roomStyles = [
+
+    {
+        src: '/sprites/choose-a-tileset/cafe.png',
+        title: 'Theme Set - Cafe',
+        variants:'cafe'
+    },
+    {
+        src: '/sprites/choose-a-tileset/casino.png',
+        title: 'Theme Set - Casino',
+        variants:'casino'
+    },
+];
+
+export default roomStyles;
