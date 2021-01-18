4ac7778d5bccb1ec6a7dceeeac2b5390a0e46202
Bing Z <bingz@me.com>
2021-01-17 23:48:36 -0700

    changed  db to local connection

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 async function db() {
     try {
-        // await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/waterCooler_db', {
-        await mongoose.connect('mongodb+srv://bing:nE76UCVtdh9X4lPI@watercooler.g9kwh.mongodb.net/waterCooler_db?retryWrites=true&w=majority', {
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/waterCooler_db', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
